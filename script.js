13a5e3f241af38a534faf3fdef1cd71f88a5d79e
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2022-08-07 12:19:24 +0100

    Add function to play a round

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
+    const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random()*choice.length)]
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    switch(playerSelection.toLowerCase()) {
+        case "rock":
+            switch(computerSelection.toLowerCase()) {
+                case "paper":
+                    return("You lose! Paper beats Rock");
+                    break;
+                case "scissors":
+                    return("You win! Rock beats Scissors");
+                    break;
+                default:
+                    return("Tie! You both selected Rock!");
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerSelection.toLowerCase()) {
+                case "scissors":
+                    return("You lose! Scissors beats Paper");
+                    break;
+                case "rock":
+                    return("You win! Paper beats Rock");
+                    break;
+                default:
+                    return("Tie! You both selected Paper!");
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerSelection.toLowerCase()) {
+                case "rock":
+                    return("You lose! Rock beats Scissors");
+                    break;
+                case "paper":
+                    return("You win! Scissors beats Paper");
+                    break;
+                default:
+                    return("Tie! You both selected Scissors!");
+                    break;
+            }
+            break;
+        default:
+            break;
+      }
+}
